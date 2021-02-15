a33f6d5856f78827b97d940eb92c16c91768b87f
hayato <haya12to17@gmail.com>
2021-02-16 00:18:00 +0900

    add GameManager

---
+++ FoxAduvanture/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameOverText;
+    [SerializeField] GameObject gameClearText;
+
+    public void GameOver()
+    {
+        gameOverText.SetActive(true);
+        Invoke("RestartScene", 1.5f);
+    }
+    public void GameClear()
+    {
+        gameClearText.SetActive(true);
+        Invoke("RestartScene", 1.5f);
+    }
+
+    void RestartScene()
+    {
+        Scene thisScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(thisScene.name);
+    }
+
+}
---
+++ FoxAduvanture/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager;
+    [SerializeField] LayerMask blockLayer;
+
+
+    public enum DIRECTION_TYPE
+    {
+        STOP,
+        LEFT,
+        RIGHT,
+    }
+    DIRECTION_TYPE direction = DIRECTION_TYPE.STOP;
+    float speed;
+    Rigidbody2D rigidbody2D;
+
+    float jumpPower = 400;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if (x == 0)
+        {
+            direction = DIRECTION_TYPE.STOP;
+        }
+        else if (x > 0)
+        {
+            direction = DIRECTION_TYPE.RIGHT;
+        }
+        else if (x < 0)
+        {
+            direction = DIRECTION_TYPE.LEFT;
+        }
+
+        if (IsGround() && Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+    }
+    void FixedUpdate()
+    {
+        switch (direction)
+        {
+            case DIRECTION_TYPE.STOP:
+                speed = 0;
+                break;
+            case DIRECTION_TYPE.RIGHT:
+                speed = 3;
+                transform.localScale = new Vector3(1, 1, 1);
+                break;
+            case DIRECTION_TYPE.LEFT:
+                speed = -3;
+                transform.localScale = new Vector3(-1, 1, 1);
+                break;
+        }
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+    }
+
+    void Jump()
+    {
+        rigidbody2D.AddForce(Vector2.up * jumpPower);
+    }
+
+    bool IsGround()
+    {
+        Vector3 leftStartPoint = transform.position - transform.right * 0.3f;
+        Vector3 endPoint = transform.position - transform.up * 0.1f;
+        Vector3 rightStartPoint = transform.position + transform.right * 0.3f;
+
+        // 確認用
+        Debug.DrawLine(leftStartPoint, endPoint);
+        Debug.DrawLine(rightStartPoint, endPoint);
+
+        return Physics2D.Linecast(leftStartPoint, endPoint, blockLayer) || Physics2D.Linecast(rightStartPoint, endPoint, blockLayer);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trap")
+        {
+            gameManager.GameOver();
+        }
+
+        if (collision.gameObject.tag == "Finish")
+        {
+            gameManager.GameClear();
+        }
+    }
+}
